e63b708aa3ec970f24f7830d0352e0c40bd6c69f
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-02-10 20:26:21 +0700

    doubly linked list

--- Data_Structure_and_Algorithms/Linked_list/28.cpp
+++ Data_Structure_and_Algorithms/Linked_list/28.cpp
@@ -37,6 +37,49 @@ void addTail(Node **tail, int x)
     *tail = tmp;
 }
 
+void insertAt(Node **head, Node **tail, int n, int k, int x)
+{
+    if (k == 0)
+    {
+        addHead(head, x);
+        return;
+    }
+
+    if (k == n)
+    {
+        addTail(tail, x);
+        return;
+    }
+
+    int mid = n / 2;
+    Node *tmp = new Node(x);
+    Node *p;
+    if (k > mid)
+    {
+        p = *tail;
+        while (--k > mid)
+        {
+            p = p->prev;
+        }
+        tmp->prev = p->prev;
+        tmp->next = p;
+        p->prev->next = tmp;
+        p->prev = tmp;
+    }
+    else
+    {
+        p = *head;
+        while (++k < mid)
+        {
+            p = p->prev;
+        }
+        tmp->prev = p->prev;
+        tmp->next = p;
+        p->prev->next = tmp;
+        p->prev = tmp;
+    }
+}
+
 void printList(Node *head)
 {
     Node *p = head;
@@ -53,15 +96,17 @@ int main()
     int n;
     cin >> n;
 
-    // get data to the head of list
+    // generate head and tail
+    
     Node *head = new Node(1);
-    Node *last = head;
+    Node *tail = head;
+
     // get data to list
     Node *p = head;
+    int tmp;
     for (int i = 2; i <= n; i++)
     {
-        addHead(&head, i);
-        addTail(&last, i);
+        addTail(&tail, i);
     }
 
     // algorithm
