d7e60eab60cd016070a4e9a7da42cd56d7bb9be4
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-02-11 18:57:21 +0700

    swap

--- Data_Structure_and_Algorithms/Search_algorithm/33-swap.cpp
+++ Data_Structure_and_Algorithms/Search_algorithm/33-swap.cpp
@@ -4,27 +4,6 @@
 
 using namespace std;
 
-int linear_search(int n, vector<int> v, int x, int firstIndex)
-{
-    if (firstIndex == 0) // browse v following 0 to n
-        for (int i = 0; i < n; i++)
-        {
-            if (v[i] == x)
-            {
-                return i;
-            }
-        }
-    else
-        for (int i = n - 1; i >= 0; i--) // if firstIndex != 0, browse v following 0 to n
-        {
-            if (v[i] == x)
-            {
-                return i;
-            }
-        }
-    return 1;
-}
-
 int main()
 {
     // input
@@ -39,12 +18,16 @@ int main()
     }
 
     // algo
-    vector<int> v2 = v;
-    sort(v2.begin(), v2.end());
-    int i = linear_search(n, v, v2[0], 0);
-    int j = linear_search(n, v, v2[n - 1], 1);
+    int maxIndex = n - 1, minIndex = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (v[n - i - 1] > v[maxIndex]) // browse v following n - 2 to 0
+            maxIndex = n - i - 1;
+        if (v[i] < v[minIndex]) // browse v following 1 to n - 1
+            minIndex = i;
+    }
     // highlight
-    swap(v[i], v[j]);
+    swap(v[minIndex], v[maxIndex]);
 
     // display
     for (int i = 0; i < n; i++)
